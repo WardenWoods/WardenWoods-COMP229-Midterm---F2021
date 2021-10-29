d6285dcd6aa31e37f1a38a2f1c24c53220f23857
Mattew Bradley <mjbradley321@gmail.com>
2021-10-29 10:39:20 -0400

    Updated Database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Matthew:password@mongodbserver.ehata.mongodb.net/books229?retryWrites=true&w=majority"
 };
